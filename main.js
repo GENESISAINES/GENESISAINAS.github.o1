73098c656642ffc7b8bbe5491a60fc506c4440cf
GENESISAINES <161649845+GENESISAINES@users.noreply.github.com>
2024-02-28 15:09:19 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let hidetexpp = document.getElementById("hidetexpp");
+let hidetex = document.getElementById(hidetex)
+hidetexpp.addEventListener("click" , toggletex)
+function toggletex() [
+    hidetex.classList.toggle("show")
+]
